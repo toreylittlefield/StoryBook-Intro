c93a775f89163b798c7cfcdb9cbc037748d2eb99
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-14 17:31:04 +0800

    update component Task.js to render inputs & boxes

--- taskbox/src/components/Task.js
+++ taskbox/src/components/Task.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-const Task = ({ task: {id, title, state}, onArchiveTask, onTaskPin }) => {
-    return (
-        <div className="list-item">
-            <input type="text" value={title} readOnly />
-        </div>
-    );
-};
+export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+      </div>
 
-export default Task;
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
